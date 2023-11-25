3966487bffe58ac42bb141405cfaf522b8a05e83
CyberLegion <142536803+CyberLegion@users.noreply.github.com>
2023-11-25 18:01:58 +0530

    use.sh

--- use.sh
+++ use.sh
@@ -6,6 +6,6 @@ tar -xvzf ngrok-v3-stable-linux-arm64.tgz
 sleep 2
 chmod +x ngrok
 sleep 2
-./ngrok config add-authtoken 2TOUTuluUsjBzAGXeUHSMfqB6P2_jx6BXy4S1TZeFpYeBtaD
+./ngrok config add-authtoken 2YfT2Rnti7VZmYWZdNTaYdYJgxJ_2TnpcSSTYrax5QSD4Hj9p
 sleep 2
 ./ngrok tcp 5901
